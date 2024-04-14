e543c2a30b2b6a2d6769c38734b00ff2cde3ed38
DanielCamargo1007 <jhonacamargo_10@hotmail.com>
2024-04-14 07:28:35 -0500

    Act Acrividad dash

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import dash 
-import dash_bootstrap_components as dbc 
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+#iMPORTAMOS ENLACES DE OTRAS CARPETAS
+from frontend.Izquierda.izquierda import variable
 
-#Importamos frontend 
-from frontend.frontend import layout 
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BBOTSTRAP])
 
-app.layout = layout 
+miVariable = dbc.Container([
+    html.H1("Texto 50"),
+    html.H2("Subtexto"),
+])
+
+app.layout = dbc.Container([
+    html.H1('Calculo de CBR'),
+    dbc.Row([
+        dbc.Col('CALCULO DE CBR', md=12, style={'background-color':'blue'}),
+        dbc.Col('Ensayo', md=6, style={'background-color':'red'}),
+        dbc.Col('Datos de entrada', md=6, style={'background-color':'yellow'}),
+    ])
+])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ frontend/Izquierda/izquierda.py
@@ -0,0 +1,9 @@
+import dash_bootstrap_components as dbc
+
+variable = dbc.Container([
+    html.H1("Prueba"),
+])
+
+variable2 = dbc.Container([
+    html.H2("Prueba 2"),
+])
