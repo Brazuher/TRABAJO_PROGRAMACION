15c8722b175d0e39a4de77cd1e1502a4a04ce334
dennysss1 <dennyssofia1234@gmail.com>
2024-04-14 17:42:09 -0500

    cambios de archivo

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-import dash 
+import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-#iMPORTAMOS ENLACES DE OTRAS CARPETAS
-from frontend.Izquierda.izquierda import variable
-
+#IMPORTAMOS LAS VARIABLES DE OTRAS CARPETAS
+from frontend.Derecha.derecha import derecha
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
-
 miVariable = dbc.Container([
     html.H1("Texto 50"),
     html.H2("Subtexto"),
 ])
 
-app.layout = dbc.Container([
+app.layout = dbc.Container (
+    [
     html.H1('Calculo de CBR'),
     dbc.Row([
         dbc.Col('CALCULO DE CBR', md=12, style={'background-color':'#FFD700'}),
-        dbc.Col('Ensayo', md=6, style={'background-color':'#DCDCDC'}),
+        dbc.Col(derecha, md=6, style={'background-color':'#DCDCDC'}),
         dbc.Col('Datos de entrada', md=6, style={'background-color':'#F0E68C'}),
-    ])
 ])
-
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+    
--- frontend/Derecha/derecha.py
+++ frontend/Derecha/derecha.py
@@ -2,20 +2,18 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 derecha = dbc.Container([
-    html.H1("Prueba"),
-    html.H2("Dato de entrada"),
-    html.H2("Grafica Esfurezo-Deformación"),
+    html.H1("¿Qué es el CBR?"),
+    html.H2("¿Para qué sirve un ensayo de CBR?"),
     ## formula cálculo a 0,1 pulgadas
     html.Div([
             html.Label('Cálculo a 0,1 pulgadas'),
             dbc.Input(id = 'valor de la deformación', value = 1 , type = 'number'),
-            html.Label(id = 'CBR 0,1"'),])
-])
-
-derecha = dbc.Container([
+            html.Label(id = 'CBR 0,1"'),
+            ]),
+    
     dbc.Row([
-        dbc.Col('Datos de entrada', md=6, style={'background-color':'red'}),
-        dbc.Col('Grafica esfuerzo deformación', md=6, style={'background-color':'blue'}),
-    ])
-
+        dbc.Col('Datos de entrada', md=6, style={'background-color':'#FFD700'}),
+        dbc.Col('Grafica esfuerzo deformación', md=6, style={'background-color':'white'}),
+    ]),
+   
 ])
