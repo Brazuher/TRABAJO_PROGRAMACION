19b5fbb850e7f9dba0fbaff0e3dbf3afce898248
DanielCamargo1007 <jhonacamargo_10@hotmail.com>
2024-04-14 11:58:24 -0500

    Row

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,6 +3,9 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 layout = dbc.Container([
-    dbc.Col('Izquierda', md=6, style={'bacjground-color':'red'}),
-    dbc.Col('Derecha', md=6, style={'bacjground-color':'red'}),
+    dbc.Row([
+        dbc.Col('Izquierda', md=6, style={'background-color':'red'}),
+        dbc.Col('Derecha', md=6, style={'background-color':'blue'}),
+    ])
+    
 ])
