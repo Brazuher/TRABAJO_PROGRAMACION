8910736f5fec1a6777143d7b52a255668fccb271
DanielCamargo1007 <jhonacamargo_10@hotmail.com>
2024-04-14 12:12:20 -0500

    distribución

--- frontend/Derecha/derecha.py
+++ frontend/Derecha/derecha.py
@@ -2,6 +2,12 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 derecha = dbc.Container([
+    html.H1("Prueba"),
+    html.H2("Dato de entrada"),
+    html.H2("Grafica Esfurezo-Deformación")
+])
+
+derecha = dbc.Container([
     dbc.Row([
         dbc.Col('Datos de entrada', md=6, style={'background-color':'red'}),
         dbc.Col('Grafica esfuerzo deformación', md=6, style={'background-color':'blue'}),
