1357f6584ca20fd5714688ad115d6c1a2c95b766
Brazuher <brazuherud@gmail.com>
2024-04-21 13:18:00 -0500

    Titulos tabla

--- frontend/Izquierda/izquierda.py
+++ frontend/Izquierda/izquierda.py
@@ -10,6 +10,11 @@ variable = dbc.Container([
         id='Tabla_CBR',
         columns=[
             {'name':'Lectura','id':'Lectura','editable': False},
+            {'name':'Carga_(lbf)','id':'Carga_(lbf)','editable': False},
+            {'name':'Deformación_(pulg)','id':'Deformación_(pulg)','editable': False},
+            {'name':'Esfuerzo','id':'Esfuerzo','editable': False},
+            {'name':'CBR_1_%','id':'CBR_1_%','editable': False},
+            {'name':'CBR_2_%','id':'CBR_2_%','editable': False},
             ],
         # data =
 
